dd9f9dbe75320bb3b989c6df211f0c60c61d9e30
Ashish <ashish@biztecno.net>
2021-09-01 21:33:45 +0530

    operators

---
+++ src/Operators.java
@@ -0,0 +1,34 @@
+
+public class Operators {
+
+	public static void main(String[] arg) {
+		// if you are having USA passport and age>18;
+//		boolean usaPassport = false;
+//		int age = 20;
+//		boolean output = usaPassport && age > 18;
+//		System.out.println("Can i vote ? " + output);
+
+//		boolean usaDL = true;
+//		boolean canDL = true;
+//		boolean output2 = usaDL || canDL;
+//		System.out.println("Can i drive ? " + output2);
+//		int age = 15;
+//		String value = age >= 18 ? "YES" : "NO";
+//		System.out.println(value);
+//		int a = 2;
+//		// int b=--a;
+//		a=a-1;
+//		int b1 = a; 
+//		
+//		
+//		
+//		// int b=a++; 
+//		int b2 = a; 
+//		a=a+1;
+//		
+//		// int b=--a;
+//		System.out.println(b);
+//		System.out.println(a);
+
+	}
+}
